c04bd8bce6efc22fa0a5c7fb7756f684a63e945f
Karl Wenninger <karl.wenninger@gmail.com>
2019-06-01 22:19:00 -0400

    Question options now displayed with letter choice: A, B, C, D. Brief delay/flash notification btwn consecutive incorrect answers

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ function loadQandAs(questionAnswers, qaIndex) {
 
     const answerOptions = questionAnswers[qaIndex].answerOptions;
 
-    document.querySelector('#answer-option-A').innerText = Object.values(answerOptions[0])
-    document.querySelector('#answer-option-B').innerText = Object.values(answerOptions[1])
-    document.querySelector('#answer-option-C').innerText = Object.values(answerOptions[2])
-    document.querySelector('#answer-option-D').innerText = Object.values(answerOptions[3])
+    document.querySelector('#answer-option-A').innerText = `${Object.keys(answerOptions[0])}: ${Object.values(answerOptions[0])}`
+    document.querySelector('#answer-option-B').innerText = `${Object.keys(answerOptions[1])}: ${Object.values(answerOptions[1])}`
+    document.querySelector('#answer-option-C').innerText = `${Object.keys(answerOptions[2])}: ${Object.values(answerOptions[2])}`
+    document.querySelector('#answer-option-D').innerText = `${Object.keys(answerOptions[3])}: ${Object.values(answerOptions[3])}`
 
     // clear UI items (displayed only on correct answer)
     answer.innerText = 'ANSWER:';
@@ -34,18 +34,25 @@ answerOptionContainer.addEventListener('click', (e) => {
     // target user click
     if (e.target.classList.contains('answer-option')); {
         userAnswer = e.target.innerText;
+        console.log(userAnswer.slice(0, 1));
     };
-    if (userAnswer == `${questionAnswers[qaIndex].answer}`) {
-        answer.innerText = `ANSWER: ${questionAnswers[qaIndex].answer} is CORRECT!!!`;
-        summary.innerText = `${questionAnswers[qaIndex].summary}`;
 
-        nextQuestionBtn.style = '';
+    if (userAnswer.slice(0, 1) == `${questionAnswers[qaIndex].answer}`) {
+        setTimeout(() => {
+            answer.innerText = `ANSWER: ${userAnswer.slice(2)} is CORRECT!!!`;
+            summary.innerText = `${questionAnswers[qaIndex].summary}`;
+            nextQuestionBtn.style = '';
+        }, 150)
     } else {
-        answer.innerText = 'ANSWER: NOPE TRY AGAIN!!!';
-        summary.innerText = '';
-        nextQuestionBtn.style.display = 'none'
+        answer.innerText = 'ANSWER:';
+        setTimeout(() => {
+            answer.innerText = 'ANSWER: NOPE TRY AGAIN!!!';
+            summary.innerText = '';
+            nextQuestionBtn.style.display = 'none';
+        }, 150)
     };
 });
+
 
 window.addEventListener('load', () => {
     loadQandAs(questionAnswers, qaIndex);
@@ -69,19 +76,19 @@ const questionAnswers = [{
             { C: 'Tim Berners-Lee' },
             { D: 'Alan Turing' }
         ],
-        answer: 'Tim Berners-Lee',
-        summary: "Berners-Lee was working at CERN, a particle physics lab in Switzerland. He noticed that visiting scientists coming from all over the world lacked a common protocol to share computer information over the internet.",
+        answer: 'C',
+        summary: "Berners-Lee was working at CERN, a particle physics lab in Switzerland. He noticed that visiting scientists coming from all over the world lacked a common protocol to share information over the internet.",
         furtherReading: `FURTHER READING: <a href='https://webfoundation.org/about/sir-tim-berners-lee/' target="_blank">webfoundation.org: Sir Tim Berners-Lee</a>`,
     },
     {
-        question: '__________ states that computer performance speeds will double every two years.',
+        question: 'This "law" states that computer performance speeds will double every two years.',
         answerOptions: [
             { A: "Ohm's Law" },
             { B: "Linus's Law" },
-            { C: "Heisenberg's Principle" },
+            { C: "Heisenberg's Law" },
             { D: "Moore's Law" }
         ],
-        answer: "Moore's Law",
+        answer: "D",
         summary: 'In 1965 Gordon Moore was director of R&D at Fairchild Semiconductor. He observed and accurately predicted that the number of components on a semiconductor chip would double every two years.'
     },
     {
@@ -92,18 +99,18 @@ const questionAnswers = [{
             { C: "Internet, easy" },
             { D: "Where do you want to go today?" }
         ],
-        answer: "Don't be evil",
+        answer: "B",
         summary: "In 2015 Google changed it's motto to 'Do the right thing'. "
     },
     {
-        question: 'In ____ a computer was used to predict the results of a U.S. presidential election. On election night it accurately forecast a landslide victory for the candidate that was NOT favored by the media and pollsters.',
+        question: 'In ____ the UNIVAC computer was used to predict the results of a U.S. presidential election. On election night it correctly forecast a landslide victory for the candidate that was NOT favored by the media and pollsters.',
         answerOptions: [
             { A: "1948" },
             { B: "1952" },
             { C: "1956" },
             { D: "1960" }
         ],
-        answer: "1952",
+        answer: "B",
         summary: "The UNIVAC..."
     },
     {
@@ -114,7 +121,7 @@ const questionAnswers = [{
             { C: "Apple" },
             { D: "Microsoft" }
         ],
-        answer: "Xerox",
+        answer: "B",
         summary: "In 1973 (nearly a decade before the first commercially available GUI computer was released) the Xerox Alto was designed and built at the company's Palo Alto Research Center. Apparently Xerox's east coast management didn't understand the revolutionary nature of the GUI technology and never tried to market it."
     },
     {
@@ -125,8 +132,8 @@ const questionAnswers = [{
             { C: "ranking web pages" },
             { D: "targeting advertisements" }
         ],
-        answer: "ranking web pages",
-        summary: `At the time search engines ranked results according to how often a searched term appeared on a web page. Google's approach was to rank results according to how many other significant pages linked to the page being evaluated.`
+        answer: "C",
+        summary: "At the time search engines ranked results by how often a searched term appeared on a web page. Google ranked results according to the number of relevant pages that linked to the page being evaluated."
     },
     {
         question: "In 1980 IBM agreed to ship it's new line of personal computers with this Microsoft software installed. This deal eventually led to the Microsoft's dominance in the PC industry.",
@@ -136,9 +143,21 @@ const questionAnswers = [{
             { C: "Internet Explorer" },
             { D: "Windows" }
         ],
-        answer: "MS-DOS",
-        summary: "IBM didn't design their PC from scratch but assembled it from existing third party components. This made it easy for competing companies to issue PC clones. Microsoft included a non-exclusive clause in the deal that allowed them to license MS-DOS, a pre-Windows, command-line operating system to other computer manufactures. "
+        answer: "B",
+        summary: "Microsoft included a non-exclusive clause in the deal that allowed them to license MS-DOS (a pre-Windows, command-line operating system) to other computer manufactures. Because IBM designed their PC using existing third party components competing companies we're able to easily and legally issue clones. MS-DOS became the default OS for many of these machines."
     },
+    {
+        question: 'Which option below is not a programming language?',
+        answerOptions: [
+            { A: "HTML" },
+            { B: "C++" },
+            { C: "JavaScript" },
+            { D: "Python" }
+        ],
+        answer: "A",
+        summary: ""
+    },
+
     {
         question: "The prototype of this app was developed as an internal communication tool at a failing podcast startup.",
         answerOptions: [
@@ -147,18 +166,18 @@ const questionAnswers = [{
             { C: "YouTube" },
             { D: "Facebook" }
         ],
-        answer: "Twitter",
+        answer: "A",
         summary: "Twitter co-founder Jack Dorsey conceived of the idea of an individual using a SMS service to communicate with a small group while working at Odeo. Odeo was a website that enabled users to create and share podcasts. It's assets were sold in 2007. "
     },
     {
-        question: "At it's root level a computer's processor is reading the state of discreet transistors: an electronic signal is read as binary 1 and a lack of signal is read as binary 0. This is refereed to as",
+        question: "At it's root level a computer's processor is reading the state of discreet transistors: an electronic signal is read as 1 and a lack of signal is read as 0. This level of code is referred to as",
         answerOptions: [
-            { A: "C" },
+            { A: "C++" },
             { B: "machine code" },
             { C: "BASIC" },
-            { D: "memory" }
+            { D: "assembly code" }
         ],
-        answer: "machine code",
+        answer: "B",
         summary: ""
     },
 
@@ -170,19 +189,19 @@ const questionAnswers = [{
             { C: "Steve Jobs" },
             { D: "Steve Kazniwookie" }
         ],
-        answer: "Steve Wozniak",
+        answer: "A",
         summary: "In 1975 Steve Wozniak designed and hand-built a computer that would become the Apple I. He and Steve Jobs assembled and sold them from a garage during the next year."
     },
     {
-        question: 'hotornot.com was an early 2000s website where users could submit pictures of themselves to be rated for attractiveness by others on a scale of 1-10. A founder of which company below built a similar site in 2003?',
+        question: 'hotornot.com was an early 2000s website where users submitted pictures of themselves to be rated for attractiveness by others. A founder of which company below built a similar site in 2003?',
         answerOptions: [
             { A: "Facebook" },
             { B: "Airbnb" },
             { C: "Uber" },
             { D: "Instagram" }
         ],
-        answer: "Facebook",
-        summary: "For facemash.com Mark Zuckerberg hacked female ID photos from Harvard University dormitory sites, presented pictures of two women side by side and asked users to pick which one was more attractive."
+        answer: "A",
+        summary: "Mark Zuckerberg's ealy site was called facemash.com. The site presented female ID photos hacked from Harvard University dormitory sites and asked users to compare and rank them."
     },
 
     {
@@ -190,10 +209,10 @@ const questionAnswers = [{
         answerOptions: [
             { A: "1970s hacker who went by the moniker 'electro_fly'" },
             { B: "fried transistor that resembled an insect" },
-            { C: "failed 1960s NASA spy satellite code named 'WASP'" },
+            { C: "failed 1960s NASA spy satellite code named 'dragon-fly'" },
             { D: "dead moth found in an electro-mechanical relay" }
         ],
-        answer: "dead moth found in an electro-mechanical relay",
+        answer: "D",
         summary: "On September 9th, 1947 programmer Grace Hopper wrote in the Harvard Mark II computer's log book 'First actual case of bug being found'. This was after a problem with the machine was traced to a moth stuck between relay contacts."
     },
     {
@@ -204,34 +223,37 @@ const questionAnswers = [{
             { C: "Bill's Gate" },
             { D: "Edison Test" }
         ],
-        answer: "Turing Test",
+        answer: "B",
         summary: 'British scientist Alan Turing is widely considered to be the father of theoretical computer science and artificial intelligence. The Turing Test was proposed in his 1950 paper "Computing Machinery and Intelligence".'
     },
     {
         question: "In 1998 the U.S. Department of Justice and the Attorneys General of twenty U.S. states sued this company for 'illegally thwarting competition in order to protect and extend its monopoly'.",
         answerOptions: [
-            { A: "Intel" },
+            { A: "Apple" },
             { B: "Facebook" },
             { C: "Amazon" },
             { D: "Microsoft" }
         ],
-        answer: "Microsoft",
+        answer: "D",
         summary: "Microsoft had bundled its Internet Explorer web browser with all Windows operating systems. They then claimed that the two pieces of software were the same product. The government's assertion was that this was done intentionally to kill competition from competing web browsers.",
     },
     {
-        question: 'In the early 2000s this was the dominant technology to display animations, games and playback for audio and video on the internet. It has largely fallen out of use since HTML5 compliant browsers support these features',
+        question: 'In the early 2000s this was the dominant technology to play animations, games, audio and video on the internet. As HTML5 compliant browsers proliferated it has largely fallen out of use.',
         answerOptions: [
             { A: "Flash" },
-            { B: "Firewire" },
+            { B: "Ruby" },
             { C: "PHP" },
             { D: "Java" }
         ],
-        answer: "Flash",
+        answer: "A",
         summary: "had to download and update the player blah blah blah"
     },
 
 ];
 
 
-
-console.log(questionAnswers.length)
+// for (let i of questionAnswers) {
+//     for (let j of i.answerOptions) {
+//         console.log(Object.keys(j))
+//     }
+// }
