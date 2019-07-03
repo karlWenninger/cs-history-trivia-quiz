959c4e3ec665e3e2cbf2396167a12607af9f649c
Karl Wenninger <karl.wenninger@gmail.com>
2019-07-02 23:03:05 -0400

    Rewote makeRandIndex(arr) and funcs it supports

--- scripts/app.js
+++ scripts/app.js
@@ -5,14 +5,23 @@ const answer = document.querySelector('#answer');
 const summary = document.querySelector('#summary');
 const nextBtn = document.querySelector('#next-btn');
 
+let randIndexArr = [];
+function makeRandIndex(arr) {
+    for (let i in questionAnswers) {
+        console.log(i)
+        randIndexArr.push(i)
+    }
+    return randIndexArr.sort((a, b) => 0.5 - Math.random());
+}
+
 // cycle thru questionsAnswers
 let qaIndex = 0;
 let answerOptions;
 
-function loadQandAs(arr, index) {
-    question.innerText = `QUESTION: ${arr[index].question}`;
+function loadQuestions(arr, qaIndex) {
+    question.innerText = `QUESTION: ${arr[randIndexArr[qaIndex]].question}`;
 
-    answerOptions = arr[index].answerOptions;
+    answerOptions = arr[randIndexArr[qaIndex]].answerOptions;
 
     // clear previous answerOptions 
     answerOptionContainer.innerHTML = '';
@@ -36,19 +45,18 @@ flyinEgg.classList = '';
 
 // get user clicked answer-option
 answerOptionContainer.addEventListener('click', (e) => {
-    // target user click
-    if (e.target.classList.contains('answer-option')); {
-        userAnswer = e.target;
-    };
+    if (e.target.className != 'answer-option pointer') {
+        return;
+    } else { userAnswer = e.target }
 
-    if (userAnswer.innerText.slice(0, 1) == `${questionAnswers[qaIndex].answer}`) {
+    if (userAnswer.innerText.slice(0, 1) == `${questionAnswers[randIndexArr[qaIndex]].answer}`) {
         clickCount++;
         answer.innerText = `ANSWER: ${userAnswer.innerText.slice(3)} is CORRECT!!!`;
         if (userAnswer.innerText.slice(3) == 'easter egg') {
             flyinEgg();
         }
         setTimeout(() => {
-            summary.innerHTML = `${questionAnswers[qaIndex].summary}`;
+            summary.innerHTML = `${questionAnswers[randIndexArr[qaIndex]].summary}`;
             nextBtn.innerText = 'NEXT QUESTION';
             nextBtn.style.display = 'block';
         }, 100)
@@ -69,46 +77,39 @@ answerOptionContainer.addEventListener('click', (e) => {
     };
 });
 
-// randomize questionAnswer index order
-let randIndexArr = [];
-
-function makeRandIndex(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        let randNum = Math.floor(Math.random() * arr.length);
-        if (randIndexArr.indexOf(randNum) == -1) {
-            randIndexArr.push(randNum);
-            return qaIndex = randNum;
-        } else { i--; }
-    }
-}
-
 // load next question, track correctAnswers 
 // btn is hidden until correct answer selected
 nextBtn.addEventListener('click', () => {
+    console.log(qaIndex , randIndexArr.length, randIndexArr[qaIndex])
     if (clickCount == 1) {
         correctAnswers++;
     }
     clickCount = 0;
 
     // if all questions completed 
-    if (randIndexArr.length == questionAnswers.length) {
+    if (qaIndex == randIndexArr.length - 1) {
+        // clear all this stuff
         question.innerText = '';
         answer.innerText = '';
         summary.innerText = '';
         answerOptionContainer.innerHTML = '';
-
+        // to display end msg splash screen
         const endMsg = document.createElement('div');
         endMsg.innerHTML = `<h4>YOU ANSWERED ${correctAnswers} OUT OF ${questionAnswers.length} CORRECTLY ON THE FIRST CLICK. CONGRATS!!!</h4><br><p>If you liked these questions here's some more stuff you'll enjoy:</p>${endGameLinks}`;
         question.appendChild(endMsg);
-        correctAnswers = 0;
-        randIndexArr = [];
-        nextBtn.innerText = 'DO QUIZ AGAIN';
 
+        nextBtn.innerText = 'DO QUIZ AGAIN';
+        nextBtn.addEventListener('click', () => {
+            correctAnswers = 0;
+            randIndexArr = [];
+            makeRandIndex(questionAnswers);
+            loadQuestions(questionAnswers, qaIndex);
+        })
         // load new question
     } else {
-        makeRandIndex(questionAnswers);
-        loadQandAs(questionAnswers, qaIndex);
-        // console.log(`randIndexArr ${randIndexArr.length} correctAnswers ${correctAnswers} ${questionAnswers.length}`)
+        qaIndex++;
+        loadQuestions(questionAnswers, qaIndex);
+        // alert('this is the else trigger')
     };
 });
 
@@ -119,6 +120,7 @@ function flyinEgg() {
 
 // add question on page load
 document.addEventListener('DOMContentLoaded', () => {
+    // opening splash screen only
     const startMsg = document.createElement('div');
     startMsg.style.textAlign = 'center';
     startMsg.style.marginTop = '';
@@ -126,10 +128,7 @@ document.addEventListener('DOMContentLoaded', () => {
                            ${questionAnswers.length} multiple choice questions<br><br>
                           (click on correct answer)`;
     question.appendChild(startMsg);
+    
     nextBtn.innerHTML = 'START<br>QUIZ';
-    nextBtn.addEventListener('click', () => {
-        makeRandIndex(questionAnswers);
-        loadQandAs(questionAnswers, qaIndex);
-
-    })
+    makeRandIndex(questionAnswers);
 });
